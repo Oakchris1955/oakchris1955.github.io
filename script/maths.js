21df2ab56e24f55026a807ddd424d5107aee9b8b
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2022-04-15 19:58:50 +0300

    Commited some files

---
+++ script/maths.js
@@ -0,0 +1,58 @@
+  function add() {
+  var num1 = document.getElementById("1st").value;
+  var num2 = document.getElementById("2nd").value;
+  var a = document.getElementById("+").checked;
+  var b = document.getElementById("-").checked;
+  var c = document.getElementById("*").checked;
+  var d = document.getElementById(":").checked;
+  var e = true;
+  var x;
+  if (a == e) {
+   x = Number(num1) + Number(num2);
+  } else if (b == e) {
+   x = Number(num1) - Number(num2);
+  } else if (c == e) {
+   x = Number(num1) * Number(num2);
+  } else if (d == e) {
+   x = Number(num1) / Number(num2);
+  } else {
+   x = "Unknown value";
+  }
+  document.getElementById("result").value = x;
+  }
+   
+  function rename() {
+  var a = document.getElementById("+").checked;
+  var b = document.getElementById("-").checked;
+  var c = document.getElementById("*").checked;
+  var d = document.getElementById(":").checked;
+  var e = true;
+  var x;
+  if (a == e) {
+   x = "Addition";
+  } else if (b == e) {
+   x = "Subtraction";
+  } else if (c == e) {
+   x = "Multiplication";
+  } else if (d == e) {
+   x = "Division";
+  } else {
+   x = "Unknown value";
+  }
+  document.getElementById("button").innerHTML = x;
+  }
+
+  function copy(a) {
+  var copyText;
+   if (a == 1) {
+    document.getElementById("result").disabled = false;
+    copyText = document.getElementById("result");
+   } else {
+    copyText = document.getElementById("notes");
+   }
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    document.execCommand("copy");
+    document.getElementById("result").disabled = true;
+    alert("Copied the notes with text: " + copyText.value);
+  }
---
+++ script/script.js
@@ -0,0 +1,62 @@
+   /* When the user clicks on the button,
+  toggle between hiding and showing the dropdown content */
+  function show() {
+    document.getElementById("myDropdown").classList.toggle("show");
+    var bef = document.getElementById("button").textContent;
+    var aft;
+    if (bef == "My creations +") {
+      aft = "My creations -";
+    } else if (bef == "My creations -") {
+      aft = "My creations +";
+    } else {
+      aft = "My creations";
+    }
+    document.getElementById("button").textContent = aft;
+  }
+
+   function cor() {
+   var x = document.getElementById("myDropdown").classList;
+   if (x == "dropdown-content show") {
+    x = "My creations -";
+    } else if (x == "dropdown-content") {
+    x = "My creations +"
+    } 
+    document.getElementById("button").textContent = x;
+   }
+ 
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+          cor();
+        }
+      }
+    }
+  }
+
+   /* Set the width of the side navigation to 250px */
+   function openNav() {
+   document.getElementById("mySidenav").style.width = "250px";
+  }
+
+   /* Set the width of the side navigation to 0 */
+   function closeNav() {
+   document.getElementById("mySidenav").style.width = "0";
+  }
+  includeHTML();
+  document.getElementById("load").remove();
+  var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("content").style.display = "block";
+}
