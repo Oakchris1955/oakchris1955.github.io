5c9385ca2e00c4d205ffd9805f2948ad085daf1d
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-07-05 14:54:14 +0300

    Implement custom handling for error pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,16 @@ import type { AppProps } from 'next/app';
 import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
-	)
+	// Check if pageProps has 'statusCode' property, which means an error has occured
+	if (Object.hasOwn(pageProps, 'statusCode')) {
+		// If yes, return the component as is
+		return <Component {...pageProps} />
+	} else {
+		// Otherwise, wrap it in our custom Layout
+		return (
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		)
+	}
 }
