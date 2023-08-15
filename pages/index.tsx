23a98a2d96b747d8f0d7c678bb677488347f606c
Oakchris1955 <80592203+Oakchris1955@users.noreply.github.com>
2023-08-15 18:41:07 +0300

    Update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 export default function Index() {
 	return (
-		<main>Index page</main>
+		<main>
+			Hello there. I am a programmer known as <b>Oakchris1955</b> who&apos;s pretty familiar with backend programming and tries to become a fullstack developer. For this reason, I have made many frontend projects, one of which is rewritting my entire website using the <a href="https://nextjs.org/">NextJS</a> framework, the results of which you are seeing here
+		</main>
 	)
 }
